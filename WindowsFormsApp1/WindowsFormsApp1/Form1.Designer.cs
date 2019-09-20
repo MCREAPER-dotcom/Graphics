17b98c3de366e93b5394ad0f28becf81dbf4342a
MCREAPER-dotcom <54661115+MCREAPER-dotcom@users.noreply.github.com>
2019-09-20 13:41:32 +0500

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.вводToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выводToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "jpg";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Файлы изображений|";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripLabel1,
+            this.toolStripButton2,
+            this.toolStripLabel2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(995, 27);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(78, 24);
+            this.toolStripButton1.Text = "Вывод";
+            this.toolStripButton1.Click += new System.EventHandler(this.ToolStripButton1_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(0, 24);
+            this.toolStripLabel1.Click += new System.EventHandler(this.ToolStripLabel1_Click_1);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(67, 24);
+            this.toolStripButton2.Text = "Ввод";
+            this.toolStripButton2.Click += new System.EventHandler(this.ToolStripButton2_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(0, 24);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(126, 24);
+            this.toolStripButton3.Text = "Информация";
+            this.toolStripButton3.Click += new System.EventHandler(this.ToolStripButton3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(995, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.вводToolStripMenuItem,
+            this.выводToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(69, 24);
+            this.toolStripMenuItem1.Text = "файлы";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem1_Click);
+            // 
+            // вводToolStripMenuItem
+            // 
+            this.вводToolStripMenuItem.Name = "вводToolStripMenuItem";
+            this.вводToolStripMenuItem.Size = new System.Drawing.Size(137, 26);
+            this.вводToolStripMenuItem.Text = "Ввод";
+            this.вводToolStripMenuItem.Click += new System.EventHandler(this.ToolStripButton2_Click);
+            // 
+            // выводToolStripMenuItem
+            // 
+            this.выводToolStripMenuItem.Name = "выводToolStripMenuItem";
+            this.выводToolStripMenuItem.Size = new System.Drawing.Size(137, 26);
+            this.выводToolStripMenuItem.Text = "Вывод";
+            this.выводToolStripMenuItem.Click += new System.EventHandler(this.ToolStripButton1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(304, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(46, 179);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(304, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "a=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "b=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "c=";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 0;
+            this.trackBar1.Location = new System.Drawing.Point(12, 249);
+            this.trackBar1.Maximum = 22;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(338, 56);
+            this.trackBar1.TabIndex = 14;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Увеличить график";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sin((x+b)*a)+c",
+            "Cos((x+b)*a)+c",
+            "(X+b)*a+c",
+            "((X+b)*a)^2+((Y+b)*a)^2=d"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 311);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 17;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "d=";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(46, 207);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(304, 22);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.Text = "2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(388, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(588, 543);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 620);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem вводToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выводToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen p = new Pen(Color.Black);
+        Point beg;
+        Bitmap map;
+        int Width = 440, Heigth = 440;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            map = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            p.StartCap = p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            // g = Graphics.FromImage(map);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            StartSqure();
+            if (comboBox1.SelectedIndex == 3) textBox4.Enabled=true;
+            figure(Convert.ToInt32(textBox1.Text), 
+                Convert.ToInt32(textBox2.Text), 
+                Convert.ToInt32(textBox3.Text), 
+                Width,Heigth
+                );     }
+        private void figure(int a, int b,int c,int w,int h) {
+            for (int i = -7; i <= w; i++) {
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    g.DrawLine(p
+                        ,i 
+                        , Start_Y((float)System.Math.Sin((((i + b) * (h / 2)) * (float)a)) * trackBar1.Value * 10 + c)
+                        , i +1
+                        , Start_Y((float)System.Math.Sin((((i + 1 + b) * (h / 2)) * (float)a)) * trackBar1.Value * 10 + c)
+                        );
+                }
+                if (comboBox1.SelectedIndex == 0)
+                    g.DrawLine(p
+                        , i + 0
+                        , Start_Y((float)System.Math.Cos((((i + b) * (h / 2)) * (float)a)) * trackBar1.Value * 10 + c)
+                        , i + 1
+                        , Start_Y(h / 2 + (float)System.Math.Cos((((i + 1 + b) * (h / 2)) * (float)a)) * trackBar1.Value * 10 + c)
+                        );
+                
+                if (comboBox1.SelectedIndex == 2)
+                {
+                    g.DrawLine(p
+                        ,Start_X(i)
+                        ,Start_Y((i+b)*a+c-5)
+                        ,Start_X(i+1)
+                        ,Start_Y((i+1+b)*a+c-5 )
+                        );
+                    g.DrawLine(p
+                        , Start_X(-i)
+                        , Start_Y((-i + b) * a + c - 5)
+                        , Start_X(-i + 1)
+                        , Start_Y((-i + 1 + b) * a + c - 5)
+                        );
+                }
+                if (comboBox1.SelectedIndex == 3) {
+                    g.DrawLine(p
+                        , Start_X((float)System.Math.Sin((((i + b) * (h / 2)) * (float)a)) * trackBar1.Value * 10 +c)-3
+                        , Start_Y((float)System.Math.Cos((((i + b) * (h / 2)) * (float)a)) * trackBar1.Value * 10 )
+                        , Start_X((float)System.Math.Sin((((i+1 + b) * (h / 2)) * (float)a)) * trackBar1.Value * 10 +c)-3
+                        , Start_Y((float)System.Math.Cos((((i+1 + b) * (h / 2)) * (float)a)) * trackBar1.Value * 10 )
+                        );
+
+                }
+
+
+            }
+
+        }
+        private float Start_X(float x) {
+
+            return x+Width/2;
+        }
+        private float Start_Y(float y) {
+            return ((-y + Heigth / 2));
+        }
+        private void StartSqure() {
+            g.DrawLine(p,0,0, 0, 440);
+            g.DrawLine(p, 0, 440, 440, 440);
+            g.DrawLine(p, 440, 440, 440, 0);
+            g.DrawLine(p, 440, 0, 0, 0);
+            g.DrawLine(p,0,220,440,220);
+            g.DrawLine(p,220,0,220,440);
+            for(int i =0; i<=Width;i++)
+            g.DrawLine(p, i+=10, 215, i , 225);
+            for (int i = 0; i <= Heigth; i++)
+                g.DrawLine(p, 215, i += 10, 225, i);
+        }
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void ToolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStripLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStripLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void ToolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (saveFileDialog1.FileName != "")
+                    map.Save(saveFileDialog1.FileName);
+            }
+
+        }
+
+        private void ToolStripLabel1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                map = (Bitmap)Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = map;
+            }
+            
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void ToolStripButton3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        private void ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrackBar2_Scroll(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 3) textBox4.Enabled = true;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            StartSqure();
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            StartSqure();
+            if (comboBox1.SelectedIndex == 3) textBox4.Enabled = true;
+            figure(Convert.ToInt32(textBox1.Text),
+                Convert.ToInt32(textBox2.Text) ,
+                Convert.ToInt32(textBox3.Text),
+                Width, 
+                Heigth
+                );
+        }
+    }
+}
